f404230201354c46e5e2c975c67f6ac25d38b082
Jackson Siro <smataweb@gmail.com>
2017-01-19 07:16:21 +0300

    Create Information.xaml.cs

---
+++ vSongBook/Information.xaml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace vSongBook
+{
+    public partial class Information : PhoneApplicationPage
+    {
+        private AppSettings settings = new AppSettings();
+		public Information()
+        {
+            InitializeComponent();
+            ContentText.Text = ContentText.Text.Replace("$", "\n");
+            ContentText.FontSize = settings.FontSizeSetting;
+		}
+    }
+}
