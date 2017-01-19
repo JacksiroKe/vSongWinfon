af0fb766e117179ba8d7fd7411ba0972aaa768fd
Jackson Siro <smataweb@gmail.com>
2017-01-19 07:31:09 +0300

    Create AssemblyInfo.cs

---
+++ vSongBook/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("vSongBook")]
+[assembly: AssemblyDescription("An App that turns your phone into a SongBook")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jackson Siro")]
+[assembly: AssemblyProduct("vSongBook")]
+[assembly: AssemblyCopyright("Copyright © Jackson Siro 2016")]
+[assembly: AssemblyTrademark("JS")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8bc9849e-ead9-4b41-9dae-e7b65b40a342")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.2.0.012")]
+[assembly: AssemblyFileVersion("1.2.0.012")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
